fa79f7efc9e82307ac7f7918b6dd984e80605216
Daisymay55 <59394429+Daisymay55@users.noreply.github.com>
2021-01-02 14:04:29 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Plague_Appy"
+    version="0.0.1",
+    author="Daisymay55",
+    description="an app that simulates plague outbreaks",
+    url="https://github.com/Daisymay55/LeedsUni-Coursework",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
