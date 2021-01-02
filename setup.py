f5446c6bd39b1cb23360ad8387d7e05095ad3087
Daisymay55 <59394429+Daisymay55@users.noreply.github.com>
2021-01-02 14:33:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Plague_App"
+    name="Plague_App",
+    version = "1.0.0",
     author="Daisymay55",
-    description="an app that simulates plague outbreaks",
+    description="an app that simulates plague outbreaks where the user can modify variables as required",
     url="https://github.com/Daisymay55/LeedsUni-Coursework",
     packages=setuptools.find_packages(),
     classifiers=[
